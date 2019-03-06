0ecf0f8523c76b304707b415c47ccf3178e19f63
dhyegofernando <dhyegofernando@gmail.com>
2019-03-06 12:14:03 -0400

    fix

--- .profile.d/postgres.sh
+++ .profile.d/postgres.sh
@@ -5,8 +5,7 @@ set -e
 [ -z "$DATABASE_URL" ] && echo "\$DATABASE_URL environment variable does not exist"
 
 IFS=':' read -r -a DATABASE_STRING <<< "$DATABASE_URL"
-# export POSTGRES_DB_USER="${DATABASE_STRING[1]//\/\/}"
-export POSTGRES_DB_USER="hcmnlvenwfuxlz"
+export POSTGRES_DB_USER=$(${DATABASE_STRING[1]} | tr -d /)
 IFS='@' read -r -a PASSWORD_HOST <<< "${DATABASE_STRING[2]}"
 export POSTGRES_DB_PASS=${PASSWORD_HOST[0]}
 export POSTGRES_DB_HOST="jdbc:postgresql://${PASSWORD_HOST[1]}:${DATABASE_STRING[3]}${POSTGRES_DB_HOST_ARGS}"
